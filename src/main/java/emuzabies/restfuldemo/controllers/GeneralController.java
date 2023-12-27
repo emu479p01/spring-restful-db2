7e016c69a0cfce486909e3888479d9f7e010f8a7
Bomb CPS <100484260+emu479p01@users.noreply.github.com>
2023-12-27 13:07:27 +0700

    add model

--- src/main/java/emuzabies/restfuldemo/controllers/GeneralController.java
+++ src/main/java/emuzabies/restfuldemo/controllers/GeneralController.java
@@ -1,6 +1,9 @@
 package emuzabies.restfuldemo.controllers;
 
+import emuzabies.restfuldemo.models.UserInfo;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -9,5 +12,10 @@ public class GeneralController {
     @GetMapping("/general/helloworld")
     public String getHelloWorld() {
         return "Hello, World!";
+    }
+    
+    @PostMapping("/general/userinfo")
+    public String setUserInfo(@RequestBody UserInfo userInfo) {
+        return "Insert " + userInfo + " to DB.";
     }
 }
---
+++ src/main/java/emuzabies/restfuldemo/models/UserInfo.java
@@ -0,0 +1,14 @@
+package emuzabies.restfuldemo.models;
+
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+@Setter
+@Getter
+@ToString
+public class UserInfo {
+
+    private String userId;
+    private String userName;
+}
